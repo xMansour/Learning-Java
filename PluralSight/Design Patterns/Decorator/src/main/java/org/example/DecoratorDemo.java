d37c57ead051a2409d68fb4a5ad8b393dfd41ba2
xMansour <MEMansour96@gmail.com>
2022-12-29 15:23:17 +0200

    Design Patterns: Decorator

--- PluralSight/Design Patterns/Decorator/src/main/java/org/example/DecoratorDemo.java
+++ PluralSight/Design Patterns/Decorator/src/main/java/org/example/DecoratorDemo.java
@@ -2,7 +2,15 @@ package org.example;
 
 public class DecoratorDemo {
     public static void main(String[] args) {
-        SandwichComponent sandwichComponent = new SandwichDecorator(new Ziet7arFolSandwichConcreteDecorator(new T7enaFolSandwichConcreteDecorator(new MkhalelFolSandwichConcreteDecorator(new FolSandwichConcreteComponent()))));
+        SandwichComponent sandwichComponent = new SandwichDecorator(
+                new Ziet7arFolSandwichConcreteDecorator(
+                        new T7enaFolSandwichConcreteDecorator(
+                                new MkhalelFolSandwichConcreteDecorator(
+                                        new FolSandwichConcreteComponent()
+                                )
+                        )
+                )
+        );
         System.out.println("sandwichComponent = " + sandwichComponent.makeSandwich());
     }
 }
