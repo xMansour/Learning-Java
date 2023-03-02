cf4fe7607cd418a0e1e9b8661a6e4e4315a640af
xMansour <MEMansour96@gmail.com>
2023-03-02 18:05:56 +0200

    Hibernate Demo CRUD

--- Spring/HibernateDemo/src/main/java/com/mansour/Main.java
+++ Spring/HibernateDemo/src/main/java/com/mansour/Main.java
@@ -8,6 +8,7 @@ import org.hibernate.cfg.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,24 +29,80 @@ public class Main {
                 .addAnnotatedClass(Student.class)
                 .buildSessionFactory();
 
+
         Session session = sessionFactory.getCurrentSession();
 
-
         try {
+
+            //Insert/Create
             Student student = new Student("Tom", "Cruse", "TomCruse@test.com");
+            Student student2 = new Student("Tom2", "Cruse2", "TomCruse@test.com");
+            Student student3 = new Student("Tom3", "Cruse3", "TomCruse@test.com");
+            Student student4 = new Student("Tom4", "Cruse4", "TomCruse@test.com");
+
             session.beginTransaction();
             session.save(student);
+            session.save(student2);
+            session.save(student3);
+            session.save(student4);
             session.getTransaction().commit();
             System.out.println("studentId = " + student.getId());
 
 
+            //Read
+            /*
             session = sessionFactory.getCurrentSession();
             session.beginTransaction();
             Student tomCruse = session.get(Student.class, student.getId());
             session.getTransaction().commit();
 
-            System.out.println("johnDoe = " + tomCruse);
+            System.out.println("johnDoe = " + tomCruse);*/
+
+            //Read
+            session.beginTransaction();
+            List<Student> students = session.createQuery("from Student").getResultList();
+            session.getTransaction().commit();
+            System.out.println("students = " + students);
+
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            students = session.createQuery("from Student where first_name = 'Mahmoud'").getResultList();
+            session.getTransaction().commit();
+            System.out.println("students = " + students);
+
+
+
+            //Update
+//            session = sessionFactory.getCurrentSession();
+//            session.beginTransaction();
+//            Student mahmoud = session.get(Student.class, students.get(0).getId());
+//            mahmoud.setEmail("Mahmoud@test.com");   //changed but still in memory
+//            session.getTransaction().commit();      //persisted
+//            System.out.println("students = " + students);
+
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            students = session.createQuery("from Student").getResultList();
+            session.getTransaction().commit();
+            System.out.println("students = " + students);
+
+            //Delete
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            session.createQuery("delete from Student where id = 2").executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("students = " + students);
+
+            //Delete
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            students.forEach(session::delete);
+            session.getTransaction().commit();
+            System.out.println("students = " + students);
+
+
         } finally {
+
             sessionFactory.close();
         }
     }
