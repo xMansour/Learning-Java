29ca56530e35941badc3937716191dbf1a80f61d
Mahmoud Mansour <memansour96@gmail.com>
2023-07-16 16:24:38 +0300

    SG: Spring Boot Actuator

---
+++ Spring/Spring Guides/Building a RESTful Web Service with Spring Boot Actuator/src/main/java/com/mansour/buildingarestfulwebservicewithspringbootactuator/springBootActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.mansour.buildingarestfulwebservicewithspringbootactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springBootActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(springBootActuatorApplication.class, args);
+    }
+
+}
---
+++ Spring/Spring Guides/Building a RESTful Web Service with Spring Boot Actuator/src/test/java/com/mansour/buildingarestfulwebservicewithspringbootactuator/springBootActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mansour.buildingarestfulwebservicewithspringbootactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class springBootActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
