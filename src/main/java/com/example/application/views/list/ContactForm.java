18a99ebfc57e9cbfdb88614444d92c28d531e504
Mahmoud Mansour <memansour96@gmail.com>
2023-07-17 12:48:46 +0300

    Vaadin Database access and services.

--- src/main/java/com/example/application/views/list/ContactForm.java
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -1,4 +1,54 @@
 package com.example.application.views.list;
 
-public class ContactForm {
+import com.example.application.data.entity.Company;
+import com.example.application.data.entity.Status;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+
+import java.util.List;
+
+public class ContactForm extends FormLayout {
+    private TextField firstName = new TextField("First Name");
+    private TextField lastname = new TextField("Last Name");
+    private EmailField email = new EmailField("Email");
+    private ComboBox<Status> status = new ComboBox<>("Status");
+    private ComboBox<Company> company = new ComboBox<>("Company");
+
+    private Button saveBtn = new Button("Save");
+    private Button deleteBtn = new Button("Delete");
+    private Button cancelBtn = new Button("Cancel");
+
+
+
+    public ContactForm(List<Company> companies, List<Status> statuses){
+        addClassName("contact-form");
+
+        company.setItems(companies);
+        company.setItemLabelGenerator(Company::getName);
+
+        status.setItems(statuses);
+        status.setItemLabelGenerator(Status::getName);
+
+        add(firstName, lastname, email, company, status, createButtonsLayout());
+
+    }
+
+    private Component createButtonsLayout() {
+        saveBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        deleteBtn.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        cancelBtn.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+        saveBtn.addClickShortcut(Key.ENTER);
+        cancelBtn.addClickShortcut(Key.ESCAPE);
+
+        return new HorizontalLayout(saveBtn, deleteBtn, cancelBtn);
+
+    }
 }
--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,6 +1,7 @@
 package com.example.application.views.list;
 
 import com.example.application.data.entity.Contact;
+import com.example.application.data.service.CRMService;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
@@ -17,24 +18,55 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 
+import java.util.Collections;
+
 @PageTitle("Contacts | VaadinCRM")
 @Route(value = "")
 public class ListView extends VerticalLayout {
     private Grid<Contact> grid = new Grid<>(Contact.class);
     private TextField filterText = new TextField();
 
-    public ListView() {
+    private ContactForm form;
+
+    private CRMService service;
+
+    public ListView(CRMService service) {
+        this.service = service;
+
         addClassName("list-view");
         setSizeFull();
         configureGrid();
-        add(getToolbar(), grid);
+        configureForm();
+        add(getToolbar(), getContent());
+        
+        updateList();
 
+    }
+
+    private void updateList() {
+        grid.setItems(service.findAllContacts(filterText.getValue()));
+    }
+
+    private Component getContent() {
+        HorizontalLayout content = new HorizontalLayout(grid, form);
+        content.setFlexGrow(2, grid);
+        content.setFlexGrow(1, form);
+        content.addClassName("content");
+        content.setSizeFull();
+        return content;
+
+
+    }
+    private void configureForm() {
+        form = new ContactForm(service.findAllCompanies(), service.findAllStatuses());
+        form.setWidth("25em");
     }
 
     private Component getToolbar() {
         filterText.setPlaceholder("Filter by name.");
         filterText.setClearButtonVisible(true);
         filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e->updateList());
 
 
         Button addContactButton = new Button("Add contact");
