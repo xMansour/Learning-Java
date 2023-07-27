17335350970e7a8abe670d741cc431456da41a4c
Mahmoud Mansour <memansour96@gmail.com>
2023-07-27 14:50:21 +0300

    Internationalization

--- Spring/SpringIOC/src/main/java/com/mansour/springioc/controller/GreetingController.java
+++ Spring/SpringIOC/src/main/java/com/mansour/springioc/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.mansour.springioc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +11,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.Locale;
 
 @Controller
-
+@PropertySource("classpath:application.properties")
 public class GreetingController {
     @Autowired
     private ResourceBundleMessageSource source;
+    @Value("${language}")
+    private String language;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public void hello() {
-        System.out.println(source.getMessage("message", null, Locale.FRANCE));
+        if (language.equals("en"))
+            System.out.println(source.getMessage("message", null, Locale.US));
+        else if (language.equals("fr"))
+            System.out.println(source.getMessage("message", null, Locale.FRANCE));
+
     }
 }
